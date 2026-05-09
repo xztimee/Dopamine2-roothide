d568442f33a57d0a775e4e8b692348e02422cc0e
Truong Quang Phong <itsmefong1408@gmail.com>
2026-05-10 03:17:13 +0700

    remove -roothide

--- BaseBin/libjailbreak/src/roothider/common.h
+++ BaseBin/libjailbreak/src/roothider/common.h
@@ -88,7 +88,6 @@ bool is_apple_internal_identifier(const char* identifier);
     @"com.Alfie.TrollInstallerX",\
     @"com.opa334.Dopamine",\
     @"com.opa334.Dopamine.roothide",\
-    @"com.opa334.Dopamine-roothide",\
     @"vn.luisepog",\
 ]
 
